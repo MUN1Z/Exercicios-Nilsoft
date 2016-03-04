11bf676d7fb2262a417ae907fe63f080ab5e01c3
sirmuniz <mrmunz2012@gmail.com>
2016-03-04 01:36:05 -0300

    Aula 01, Exercicio 05 de java.

---
+++ Java/src/exercicios/aula01/Exercicio05.java
@@ -0,0 +1,27 @@
+/*
+ * Faça um Programa que converta metros para centímetros.
+ */
+package exercicios.aula01;
+
+import java.util.Scanner;
+
+/**
+ * Exericicio05 [Classe]
+ *
+ * @author Felipe Muniz, 2016, Nilsoft
+ */
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        float metro, centimetro;
+
+        System.out.println("Digite quantos metros quer converter para centimetros:");
+        metro = entrada.nextInt();
+
+        System.out.println("\n" + metro + " metros é igual a " + metro / 0.010000 +" centimetros");
+
+    }
+}
