de4e62d7d8a63a4369c9d85ca00bb501ed7bcff5
sirmuniz <mrmunz2012@gmail.com>
2016-03-04 01:29:14 -0300

    Aula 01, Exercicio 04 de java.

---
+++ Java/src/exercicios/aula01/Exercicio04.java
@@ -0,0 +1,32 @@
+/*
+ * Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+ */
+package exercicios.aula01;
+
+import java.util.Scanner;
+
+/**
+ * Exericicio04 [Classe]
+ *
+ * @author Felipe Muniz, 2016, Nilsoft
+ */
+public class Exercicio04 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        float nota[] = new float[4];
+        float media;
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Digite a "+(i+1)+"ª nota: ");
+            nota[i] = entrada.nextInt();
+        }
+        
+        media = (nota[0] + nota[1] + nota[2] + nota[3]) / 4;
+        
+        System.out.println("\nA média das Notas é: "+media);
+        
+    }
+}
