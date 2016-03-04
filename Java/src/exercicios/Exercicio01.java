623385d0e60a724525202a9d855c099d78c82402
sirmuniz <mrmunz2012@gmail.com>
2016-03-04 01:01:14 -0300

    Exercicio 01 de java da nilsoft

---
+++ Java/src/exercicios/Exercicio01.java
@@ -0,0 +1,16 @@
+/*
+ * Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+ */
+package exercicios;
+
+/**
+ * Exericicio01 [Classe]
+ * @author Felipe Muniz, 2016, Nilsoft
+ */
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        System.out.println("Alô Mundo!!");
+    }
+    
+}
